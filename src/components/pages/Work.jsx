08fd58ecc7ca458c44264602db1103c8ad237016
Bamontan <baptiste.demontangon@gmail.com>
2021-04-13 15:24:59 +0200

    moved to Link component for page links

--- src/components/pages/Work.jsx
+++ src/components/pages/Work.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 //same as contact page, I have an array of projects
 //so I can modify the html of all project in one place and add/modify one very simply
@@ -6,16 +7,23 @@ const Projects = [
   {
     name: "MineSweeper",
     imageName: "minesweeper.png",
-    link: "https://baptistemontan.github.io/my-portfolio/minesweeper.html",
+    link: "minesweeper",
     github: "",
-    description: "This is a MineSweeper Game (link is to my old portfolio)"
+    description: "This is a MineSweeper Game",
   },
   {
     name: "PathFinder",
     imageName: "pathfinder.png",
     link: "https://baptistemontan.github.io/pathfinder-react/",
     github: "https://github.com/Baptistemontan/pathfinder-react",
-    description: "This is a Pathfinder Visualizer that I made under React"
+    description: "This is a Pathfinder Visualizer that I made under React",
+  },
+  {
+    name: "WebApp",
+    imageName:"",
+    link: "https://baptistemontan.github.io/webapp/",
+    github: "https://github.com/Baptistemontan/webapp",
+    description: "This is a web application made for a truck monitoring project using Google Map API"
   }
 ];
 
@@ -23,28 +31,31 @@ export default function Work() {
   return (
     <main id="work">
       <h1 className="lg-heading">
-        My<span class="text-secondary"> Work</span>
+        My<span className="text-secondary"> Work</span>
       </h1>
-      <h2 className="sm-heading">here is some of my projects :</h2>
+      <h2 className="sm-heading">Here is some of my projects :</h2>
       <div className="projects">
-        {Projects.map(projet => {
+        {Projects.map((projet) => {
           return (
             <div className="item" key={projet.name}>
-              <a className="square" href={projet.link}>
-                <img src={"./images/" + projet.imageName} alt="project" />
+              <Link className="square" to={projet.link}>
+                <img
+                  src={"./portfolio/images/" + projet.imageName}
+                  alt="project"
+                />
                 <div>
                   <div>
                     <p>{projet.description}</p>
                   </div>
                 </div>
-              </a>
-              <a href={projet.link} className="btn-light">
+              </Link>
+              <Link to={projet.link} className="btn-light">
                 <i className="fas fa-eye"></i>
                 {projet.name}
-              </a>
-              <a href={projet.github} className="btn-dark">
+              </Link>
+              <Link to={projet.github} className="btn-dark">
                 <i className="fab fa-github"></i>GitHub
-              </a>
+              </Link>
             </div>
           );
         })}
